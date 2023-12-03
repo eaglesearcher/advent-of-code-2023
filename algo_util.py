d9aa23d3917ccb9526bb8cf58ca3efa2aea5323a
eaglesearcher <josh.wetherington@gmail.com>
2023-12-03 11:16:14 -0500

    new alg for adj 2d, refactor day3

--- algo_util.py
+++ algo_util.py
@@ -77,7 +77,66 @@ def num_to_word(value):
         token = str(value)
     return token
 
+def get_adj_cells_2d(grid, row_idx, col_idx):
+    
+    adj_cells = [[None,None,None],[None,None,None],[None,None,None]]
+    adj_dict = {'L':None, 'C': None, 'R':None,
+                'UL':None, 'U':None, 'UR':None,
+                'DL':None,'D':None,'DR':None}
+    adj_ref = {'L':None, 'C': None, 'R':None,
+                'UL':None, 'U':None, 'UR':None,
+                'DL':None,'D':None,'DR':None}
+    # above left, above, above right
+    # left, This, right
+    # below left, below, below right
+    
+    # checks if the calling cell is on an edge
+    # adj_dict & adj_ref returns None for out-of-bounds cells
 
+    max_rows = len(grid)
+    max_cols = len(grid[row_idx])
+    
+    if col_idx > 0:
+        adj_cells[1][0] = grid[row_idx][col_idx - 1]
+        adj_dict['L'] = grid[row_idx][col_idx - 1]
+        adj_ref['L'] = (row_idx, col_idx - 1)
+    
+    if col_idx < max_rows - 1:
+        adj_cells[1][2] = grid[row_idx][col_idx + 1]
+        adj_dict['R'] = grid[row_idx][col_idx + 1]
+        adj_ref['R'] = (row_idx, col_idx + 1)
+    
+    if row_idx > 0: # not on the first row
+        adj_cells[0][1] = grid[row_idx - 1][col_idx]
+        adj_dict['U'] = grid[row_idx - 1][col_idx]
+        adj_ref['U'] = (row_idx - 1, col_idx)
+        
+        if col_idx > 0: # not on the first col
+            adj_cells[0][0] = grid[row_idx - 1][col_idx - 1]
+            adj_dict['UL'] = grid[row_idx - 1][col_idx - 1]
+            adj_ref['UL'] = (row_idx - 1, col_idx - 1)
+        
+        if col_idx < max_cols - 1:
+            adj_cells[0][2] = grid[row_idx - 1][col_idx + 1]
+            adj_dict['UR'] = grid[row_idx - 1][col_idx + 1]
+            adj_ref['UR'] = (row_idx - 1, col_idx + 1)
+
+    if row_idx < max_rows - 1: # not on the last row
+        adj_cells[2][1] = grid[row_idx + 1][col_idx]
+        adj_dict['D'] =  grid[row_idx + 1][col_idx]
+        adj_ref['D'] = (row_idx + 1, col_idx)
+        
+        if col_idx > 0: # not on the first col
+            adj_cells[2][0] = grid[row_idx + 1][col_idx - 1]
+            adj_dict['DL'] = grid[row_idx + 1][col_idx - 1]
+            adj_ref['DL'] = (row_idx + 1, col_idx - 1)
+        
+        if col_idx < max_cols - 1:
+            adj_cells[2][2] = grid[row_idx + 1][col_idx + 1]
+            adj_dict['DR'] = grid[row_idx + 1][col_idx + 1]
+            adj_ref['DR'] = (row_idx + 1, col_idx + 1)
+    
+    return adj_dict, adj_ref
 
 
 
