b80fa14b9811a53518a2f215a4ca8d02894daa93
Eagle <josh.wetherington@gmail.com>
2023-12-07 10:24:14 -0500

    d7 p1 complete

--- day7.py
+++ day7.py
@@ -1,61 +1,53 @@
 import numpy as np
 import file_io as fio
 import algo_util as alg
+from operator import itemgetter
 
 day_num = 7
-input_type = 0 # 0 = test, 1 = input
+input_type = 1 # 0 = test, 1 = input
 
 def get_hand_type(hand):
     
     # 5K:7, 4K:6, FH:5, 3K:4, 2P:3, 1P:2, High:1
-    cards = ['A','K','Q','J','T','9','8','7','6','5','4','3','2']
     card_dict = {}
-    for card in cards:
-        card_dict[card] = 0
         
     for card in hand:
-        card_dict[card] += 1
-        
-    pair_3 = 0
-    pair_2 = 0
-    
-    for card in card_dict:
-        if card_dict[card] == 5:
-            hand_type = 7 # 5 of a kind
-            break
-        if card_dict[card] == 4:
-            hand_type = 6 # 4 of a kind
-            break
-        if card_dict[card] == 3:
-            pair_3 = 1
-        if card_dict[card] == 2:
-            pair_2+= 1
-    if pair_3 == 1:
-        if pair_2 == 1:
-            hand_type = 5 # full house
+        if card in card_dict:
+            card_dict[card] += 1
         else:
-            hand_type = 4 # three of a kind
-    elif pair_2 == 2:
-        hand_type = 3 # two pair
-    elif pair_2 == 1: # one pair
+            card_dict[card] = 1
+            
+    counts = sorted(card_dict.values(), reverse = True)
+    
+    if counts[0] == 5: # five of a kind
+        hand_type = 7
+    elif counts[0] == 4: # four of a kind
+        hand_type = 6
+    elif counts[0] == 3 and counts[1] == 2: # full house
+        hand_type = 5
+    elif counts[0] == 3: # three of a kind
+        hand_type = 4
+    elif counts[0] == 2 and counts[1] == 2: # two pair
+        hand_type = 3
+    elif counts[0] == 2: # one pair
         hand_type = 2
     else:
         hand_type = 1
     
     return hand_type
 
+def init_card_values():
+    cards = 'AKQJT98765432'
+    card_value = {}
+    for idx, card in enumerate(cards):
+        card_value[card] = 13-idx
+    return card_value
 
-
-# def compare_hands(hand_1, hand_2):
-    
-    
-    
-    
-    
-    
-
-
-
+def tokenize(hand, card_values):
+    values = [get_hand_type(hand)]
+    for card in hand:
+        values.append(card_values[card])
+    return values
 
 
 def main():
@@ -64,20 +56,36 @@ def main():
         return
     
     # --- add code here! ---
+    card_values = init_card_values()
+    
+    
     hands = [i.split()[0] for i in file_contents]
     bids = [i.split()[1] for i in file_contents]
     
     card_dict = {}
-    for idx, bid in enumerate(bids):
-        card_dict[hands[idx]] = int(bid)
+    for idx, hand in enumerate(hands):
+        card_dict[hand] = int(bids[idx])
     
-    print(card_dict)
-    # print(bids)
+    # pattern: decorate, sort, undecorate
+    # print(card_dict)
+    decorated = [(tokenize(hand, card_values), hand) for hand in card_dict.keys()]
+    decorated.sort()
+    sorted_hands = [i[1] for i in decorated]
+    
+    sum_value = 0
+    for idx, hand in enumerate(sorted_hands):
+        # print(hand, card_dict[hand])
+        sum_value += card_dict[hand]*(idx+1)
+    
+    # value = [card_dict[i]*(idx+1) for idx,i in enumerate(sorted_hands)]
+    # sum_value = sum(value)
 
+
+    
 
     # ----------------------
     
-    part1 = 0
+    part1 = sum_value
     part2 = 0
 
 
